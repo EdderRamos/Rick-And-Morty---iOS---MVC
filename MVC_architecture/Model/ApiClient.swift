1fd0d1973e7cf3f050fc5d49be4c3d608cd293c2
leeJhonatan <lee_jhonatan_22@hotmail.com>
2024-06-15 23:34:21 -0500

    Initial Commit

---
+++ MVC_architecture/Model/ApiClient.swift
@@ -0,0 +1,13 @@
+//
+//  ApiClient.swift
+//  MVC_architecture
+//
+//  Created by Edder Jhonatan Ramos Choquehuayta on 15/06/24.
+//
+
+import Foundation
+final class ListOfCharactersApiClient {
+    func getListOfCharacters() async z{
+        let url = URL(string: "")
+    }
+}
---
+++ MVC_architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_architecture
+//
+//  Created by Edder Jhonatan Ramos Choquehuayta on 15/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
